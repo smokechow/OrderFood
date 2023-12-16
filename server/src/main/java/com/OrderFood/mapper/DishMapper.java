380a42f70bbc087bf4818fe2290235814ba33737
jjbffjoo <56024216+jjbffjoo@users.noreply.github.com>
2023-12-16 17:24:12 +0800

    OrderFood

---
+++ server/src/main/java/com/OrderFood/mapper/DishMapper.java
@@ -0,0 +1,16 @@
+package com.OrderFood.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface DishMapper {
+
+    /**
+     * 根据分类id查询菜品数量
+     * @param categoryId
+     */
+    @Select("select count(id) from dish where category_id = #{categoryId}")
+    Integer countByCategoryId(Long categoryId);
+
+}
---
+++ server/src/main/java/com/OrderFood/mapper/SetmealMapper.java
@@ -0,0 +1,16 @@
+package com.OrderFood.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface SetmealMapper {
+
+    /**
+     * 根据分类id查询套餐的数量
+     * @param id
+     */
+    @Select("select count(id) from setmeal where category_id = #{categoryId}")
+    Integer countByCategoryId(Long id);
+
+}
